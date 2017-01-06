0052627e3541f29e87d6195ca24602ca0251ffd5
ToruNiina <niina.toru.68u@gmail.com>
2017-01-06 14:41:56 +0900

    use thread in mandelbrot

--- mandelbrot/mandelbrot.cu
+++ mandelbrot/mandelbrot.cu
@@ -7,26 +7,29 @@ struct Complex
 };
 
 template<typename realT>
-__device__ Complex<realT> add(const Complex<realT> a, const Complex<realT> b)
+__device__ __forceinline__
+Complex<realT> add(const Complex<realT> a, const Complex<realT> b)
 {
     return Complex<realT>{a.r + b.r, a.i + b.i};
 }
 
 template<typename realT>
-__device__ Complex<realT> mul(const Complex<realT> a, const Complex<realT> b)
+__device__ __forceinline__
+Complex<realT> mul(const Complex<realT> a, const Complex<realT> b)
 {
     return Complex<realT>{a.r * b.r - a.i * b.i, a.r * b.i + b.r * a.i};
 }
 
 template<typename realT>
-__device__ Complex<realT>
-mandelbrot_step(const Complex<realT> z, const Complex<realT> c)
+__device__ __forceinline__ 
+Complex<realT> mandelbrot_step(const Complex<realT> z, const Complex<realT> c)
 {
     return add(mul(z, z), c);
 }
 
 template<typename realT>
-__device__ realT abs(const Complex<realT> z)
+__device__ __forceinline__
+realT abs(const Complex<realT> z)
 {
     return z.r * z.r + z.i * z.i;
 }
@@ -50,14 +53,16 @@ __global__ void
 write_mandelbrot(
         bool* result,
         const realT rmin, const realT rmax, const realT imin, const realT imax,
-        const std::size_t th)
+        const std::size_t th, const std::size_t x_dim, const std::size_t y_dim)
 {
-    const int x = blockIdx.x;
-    const int y = blockIdx.y;
-    const int offset = x + gridDim.x * y;
+    const std::size_t x = threadIdx.x + blockIdx.x * blockDim.x;
+    const std::size_t y = threadIdx.y + blockIdx.y * blockDim.y;
 
-    const realT r = rmin + (rmax - rmin) / 1000 * x;
-    const realT i = imin + (imax - imin) / 1000 * y;
+    const std::size_t offset = x + gridDim.x * blockDim.x * y;
+    if(offset > x_dim * y_dim) return;
+
+    const realT r = rmin + (rmax - rmin) / x_dim * x;
+    const realT i = imin + (imax - imin) / y_dim * y;
     const Complex<realT> z{r, i};
 
     result[offset] = is_mandelbrot(z, th);
@@ -67,33 +72,36 @@ write_mandelbrot(
 
 int main()
 {
-    const double rmin = -2.0;
-    const double rmax =  1.0;
-    const double imin = -1.5;
-    const double imax =  1.5;
-    const std::size_t threshold = 200;
+    const double rmin = -1.05;
+    const double rmax = -1.0;
+    const double imin =  0.23;
+    const double imax =  0.28;
+    const std::size_t threshold = 2000;
+    const std::size_t x_dim = 4096;
+    const std::size_t y_dim = 4096;
 
     bool* mandelbrot;
-    cudaError_t ermalloc = cudaMalloc((void**)&mandelbrot, 1000 * 1000);
+    cudaError_t ermalloc = cudaMalloc((void**)&mandelbrot, x_dim * y_dim);
     assert(ermalloc == 0);
 
-    dim3 grid(1000, 1000);
-    write_mandelbrot<double><<<grid, 1>>>(
-            mandelbrot, rmin, rmax, imin, imax, threshold);
+    dim3 blocks(x_dim/32, y_dim/32);
+    dim3 threads(32, 32);
+    write_mandelbrot<double><<<blocks, threads>>>(
+            mandelbrot, rmin, rmax, imin, imax, threshold, x_dim, y_dim);
 
-    bool result[1000 * 1000];
+    bool *result = new bool[x_dim * y_dim];
     cudaError_t ercpy = cudaMemcpy(
-            result, mandelbrot, 1000 * 1000, cudaMemcpyDeviceToHost);
+            result, mandelbrot, x_dim * y_dim, cudaMemcpyDeviceToHost);
     assert(ercpy == 0);
 
     cudaFree(mandelbrot);
 
-    png::image<png::rgb_pixel> image(1000, 1000);
-    for(std::size_t i=0; i<1000; ++i)
+    png::image<png::rgb_pixel> image(x_dim, y_dim);
+    for(std::size_t i=0; i<x_dim; ++i)
     {
-        for(std::size_t j=0; j<1000; ++j)
+        for(std::size_t j=0; j<y_dim; ++j)
         {
-            std::size_t offset = i + 1000 * j;
+            std::size_t offset = i + x_dim * j;
             if(result[offset])
                 image[i][j] = png::rgb_pixel(255, 0, 0);
             else
